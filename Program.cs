172bb1b044264dc038d7034f996abb272de056aa
Davi Jose Batista Lima de Oliveira <davibatistalimadeoliveira@gmail.com>
2022-09-17 10:27:20 -0300

    soma dos 2 numeros concluida

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int primeironumero, segundonumero, soma;
+
+Console.WriteLine("soma dos numeros.\n");
+
+Console.Write("digite seu priemiro numero: ");
+primeironumero = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("digite seu segundo numero: ");
+segundonumero = Convert.ToInt32(Console.ReadLine());
+
+soma = primeironumero + segundonumero;
+
+Console.WriteLine($"\nsoma: {soma}");
